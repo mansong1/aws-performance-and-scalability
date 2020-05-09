d4d259de85a19528055911742a0cac9485a51701
mansong1 <martin.ansong@gmail.com>
2020-05-09 14:48:12 +0100

    Add aws_ami resource and use variables as that's the only supported way to reference variables now

--- main.tf
+++ main.tf
@@ -1,27 +1,33 @@
 provider "aws" {
     region = var.region
+    shared_credentials_file = var.shared_credentials_file
     version = "~> 2.61"
 }
 
-data "aws_ami" "ubuntu" {
+data "aws_ami" "amazon_linux" {
   most_recent = true
+  owners      = ["137112412989"] # Amazon
 
   filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+    name = "name"
+
+    values = [
+      "amzn-ami-hvm-*-x86_64-gp2",
+    ]
   }
 
   filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
+    name = "owner-alias"
 
-  owners = ["099720109477"] # Canonical
+    values = [
+      "amazon",
+    ]
+  }
 }
 
 resource "aws_instance" "UdacityT2" {
     count = 4
-    ami = ${data.aws_ami.}
+    ami = data.aws_ami.amazon_linux.id
     instance_type = "t2.micro"
     tags = {
         Name = "Udacity T2 - ${count.index}"
@@ -30,7 +36,7 @@ resource "aws_instance" "UdacityT2" {
 
 resource "aws_instance" "UdacityM4" {
     count = 2
-    ami =
+    ami = data.aws_ami.amazon_linux.id
     instance_type = "m4.large"
     tags = {
         Name = "Udacity M4 - ${count.index}"
