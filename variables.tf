332dd03281330e6cac19adce1d86f1953166c0c1
mansong1 <martin.ansong@gmail.com>
2020-05-09 14:48:39 +0100

    add variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "region" {
+    description = "The AWS region to use"
+    default = "us-east-1"
+}
+
+variable "shared_credentials_file" {
+  description = "Credentials files to use for authentication"
+}
